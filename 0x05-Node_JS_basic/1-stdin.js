f5eb44c1b672379af32cfb2f44123f0ac9ec0a19
Shinene <cherishlyte13@gmail.com>
2023-08-22 19:09:44 +0100

    Using process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,13 @@
 process.stdout.write("Welcome to Holberton School, what is yoyr name?\n");
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
+process.stdin.on('readable', () => {
+  const cherish = process.stdin.read();
+
+  if (cherish) {
+    process.stdout.write("Your name is: ${cherish}");
+  }
+});
+
+  process.stdin.on("end", () => {
   process.stdout.write("This important software is now closing\n");
-  process.exit();
 });
