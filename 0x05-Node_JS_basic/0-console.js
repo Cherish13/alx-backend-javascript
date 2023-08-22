c3a299b2fffde1c8faeeedded25269f2c51848b1
Shinene <cherishlyte13@gmail.com>
2023-08-22 18:46:21 +0100

    executing node.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,6 @@
 /**
- * displays a string argument to the console.
- * @param {string} message. The message to display.
+ * displays a message on the STDOUT.
+ * @param {String} msg. The message to display.
  * @author Ossai Cherish Nneka <https://github.com/Cherish13>
  */
 const displayMessage = (msg) => {
